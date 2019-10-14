7eab9b0aee54672d2406f5d15f368a1c1a14ef8c
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-14 10:39:15 -0600

    Add test to potter api service test to verify content of results returned and format from facade

--- spec/service/potter_house_search_results_spec.rb
+++ spec/service/potter_house_search_results_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'potter api service' do
     service = HogwartsHousesSearchResults.new("Gryffindor")
     array_of_members_data = service.members
     expect(service).to be_an_instance_of(HogwartsHousesSearchResults)
-
     expect(array_of_members_data).to be_a(Array)
     expect(array_of_members_data.first).to be_a(Member)
+    expect(array_of_members_data.first.name).to be_a(String)
     expect(array_of_members_data.count).to eq(41)
   end
 end
